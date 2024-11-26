0093fd46cf8f2c3b787a54fffaf4113e1bb679d5
yoshioka shuto <c0b23147ec@edu.teu.ac.jp>
2024-11-26 16:55:22 +0900

    演習3:画像切り替え

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -41,8 +41,8 @@ def game_over(screen: pg.Surface) -> None:
     screen.blit(black_img, [0, 0])  # 背景を黒色に
 
     screen.blit(txt, [WIDTH / 2 - txt.get_width() / 2, HEIGHT / 2 - txt.get_height() / 2])  # GameOverの表示
-    screen.blit(kk8_img, [WIDTH / 2 - txt.get_width() / 2 - kk8_img.get_width()/2 - 30, HEIGHT / 2 + txt.get_height() / 2 - kk8_img.get_height()])  #こうかとんの表示(左)
-    screen.blit(kk8_img, [WIDTH / 2 + txt.get_width() / 2 + kk8_img.get_width()/2, HEIGHT / 2 + txt.get_height() / 2 - kk8_img.get_height()])  #工科トンの表示(右)
+    screen.blit(kk8_img, [WIDTH / 2 - txt.get_width() / 2 - kk8_img.get_width() / 2 - 30, HEIGHT / 2 + txt.get_height() / 2 - kk8_img.get_height()])  #こうかとんの表示(左)
+    screen.blit(kk8_img, [WIDTH / 2 + txt.get_width() / 2 + kk8_img.get_width() / 2, HEIGHT / 2 + txt.get_height() / 2 - kk8_img.get_height()])  #工科トンの表示(右)
     pg.display.update()
     time.sleep(5)
 
@@ -63,6 +63,20 @@ def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
         bb_circle.append(bb_img)
 
     return bb_circle, accs
+
+def get_kk_img(sum_mv: tuple[int, int]) -> pg.Surface:
+    kk_img = pg.image.load("fig/3.png")
+    kk_imgs = {(-5, 0): pg.transform.rotozoom(kk_img, 0, 0.9),
+               (-5, +5): pg.transform.rotozoom(kk_img, 45, 0.9),
+               (0, 5):pg.transform.rotozoom(pg.transform.flip(kk_img,True, False), 270, 0.9),
+               (5, 5):pg.transform.rotozoom(pg.transform.flip(kk_img,True, False), 315, 0.9),
+               (5, 0):pg.transform.rotozoom(pg.transform.flip(kk_img,True, False), 0, 0.9),
+               (5, -5):pg.transform.rotozoom(pg.transform.flip(kk_img,True, False), 45, 0.9),
+               (0, -5):pg.transform.rotozoom(pg.transform.flip(kk_img,True, False), 90, 0.9),
+               (-5, -5): pg.transform.rotozoom(kk_img, 315, 0.9),}
+    return kk_imgs[sum_mv]
+
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -98,23 +112,25 @@ def main():
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
         kk_rct.move_ip(sum_mv)
+        if tuple(sum_mv) != (0, 0):
+            kk_img = get_kk_img(tuple(sum_mv))
         if check_bound(kk_rct) != (True, True):  # こうかとんの画面内判定
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
 
-        avx = vx*bb_accs[min(tmr//500, 9)]
-        avy = vy*bb_accs[min(tmr//500, 9)]
+        avx = vx*bb_accs[min(tmr//500, 9)]  # こうかとんの加速(x)
+        avy = vy*bb_accs[min(tmr//500, 9)]  # こうかとんの加速(y)
 
-        bb_rct.move_ip(avx, avy)
+        bb_rct.move_ip(avx, avy)  # 加速度の変更
         
-        bb_img = bb_imgs[min(tmr//500, 9)]
+        bb_img = bb_imgs[min(tmr//500, 9)]  # ばくだんの拡大
         
         yoko, tate = check_bound(bb_rct)  # ばくだんの画面内判定
         if not yoko:
             vx *= -1
         if not tate:
             vy *=-1
-            
+
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
