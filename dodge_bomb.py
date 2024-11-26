01b622cf5d348e7351e9592d8d0c553e3d7c15a5
yoshioka shuto <c0b23147ec@edu.teu.ac.jp>
2024-11-26 16:22:31 +0900

    演習1, 2:GameOver処理,　爆弾の加速

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import random
 import sys
+import time
 import pygame as pg
 
 
@@ -22,6 +23,47 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
         tate = False
     return yoko, tate
 
+
+def game_over(screen: pg.Surface) -> None:
+    """
+    ゲームオーバー時に，半透明の黒い画面上に「Game Over」と表示し，
+    泣いているこうかとん画像を貼り付ける関数
+    引数:screen
+    戻り値:なし
+    """
+    kk8_img = pg.image.load("fig/8.png")
+    fonto = pg.font.Font(None, 80)
+    txt = fonto.render("Game Over", True, (255, 255, 255))
+
+    black_img = pg.Surface((WIDTH, HEIGHT))
+    pg.draw.rect(black_img, (0, 0, 0), (0, 0, WIDTH, HEIGHT), 1)
+    black_img.set_alpha(150)
+    screen.blit(black_img, [0, 0])  # 背景を黒色に
+
+    screen.blit(txt, [WIDTH / 2 - txt.get_width() / 2, HEIGHT / 2 - txt.get_height() / 2])  # GameOverの表示
+    screen.blit(kk8_img, [WIDTH / 2 - txt.get_width() / 2 - kk8_img.get_width()/2 - 30, HEIGHT / 2 + txt.get_height() / 2 - kk8_img.get_height()])  #こうかとんの表示(左)
+    screen.blit(kk8_img, [WIDTH / 2 + txt.get_width() / 2 + kk8_img.get_width()/2, HEIGHT / 2 + txt.get_height() / 2 - kk8_img.get_height()])  #工科トンの表示(右)
+    pg.display.update()
+    time.sleep(5)
+
+
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    """
+    サイズの異なる爆弾Surfaceを要素としたリストと加速度リストを返す
+    引数はなし
+    戻り値：tuple[list[pg.Surface], list[int]]
+    """
+    accs = [a for a in range(1, 11)]
+
+    bb_circle = []
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_img.set_colorkey((0, 0, 0))
+        bb_circle.append(bb_img)
+
+    return bb_circle, accs
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -37,12 +79,16 @@ def main():
     vx , vy = 5, 5  # 爆弾の移動
     clock = pg.time.Clock()
     tmr = 0
+
+    bb_imgs, bb_accs = init_bb_imgs()
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         if kk_rct.colliderect(bb_rct):
-            return #ゲームオーバー
+            game_over(screen)
+            return
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
@@ -55,12 +101,20 @@ def main():
         if check_bound(kk_rct) != (True, True):  # こうかとんの画面内判定
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy)
+
+        avx = vx*bb_accs[min(tmr//500, 9)]
+        avy = vy*bb_accs[min(tmr//500, 9)]
+
+        bb_rct.move_ip(avx, avy)
+        
+        bb_img = bb_imgs[min(tmr//500, 9)]
+        
         yoko, tate = check_bound(bb_rct)  # ばくだんの画面内判定
         if not yoko:
             vx *= -1
         if not tate:
             vy *=-1
+            
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
